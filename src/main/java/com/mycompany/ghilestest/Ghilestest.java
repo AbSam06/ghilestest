f2a7cf4ff3f79d00a247e684cf88bdf74c9a32b4
AbSam06 <Samyabd2019@gmail.com>
2025-02-21 11:10:36 -0500

    first commit

---
+++ src/main/java/com/mycompany/ghilestest/Ghilestest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ghilestest;
+
+/**
+ *
+ * @author 2417011
+ */
+public class Ghilestest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
